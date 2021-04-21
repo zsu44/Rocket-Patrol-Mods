e4f3d5aae8d927f60d34731e3b586bf19f0cb905
Zhiheng Su <zsu44@ucsc.edu>
2021-04-20 21:17:47 -0700

    credit and scores breakdown

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
+/*
+Zhiheng Su, CMPM120-01
+'Hunting Game 2021'
+4/19/2021
+Time Taken: 24 hrs
 
+randomize "ships" -- check. (5)
+background music -- check. (5)
+display time -- check.(10) (Help from TA Tessa Eagle)
+add time for hits -- check (20) (Thomas and Jared helped from discord)
+full reskin -- check(60) (new assets; new sfx; new UI; new menu screen)
+100 points
+
+//sound effects from mixkit.com, The Mushroom Kingdom, www.fesliyanstudios.com, www.storyblocks.com
+
+*/
 let config = {
     type: Phaser.CANVAS,
     width:640,
